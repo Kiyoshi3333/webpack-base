e084f84752d4eed0500040e4e4c4e54619d9e865
Kiyoshi3333 <eemcsquared@gmail.com>
2021-12-17 22:29:40 +0900

    Refactor code

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,25 +2,16 @@ import {useEffect, useState,useCallback, useRef} from "react";
 
 const Game = () => {
   const countRef =  useRef(0)
-  const [someState,setSomeState] = useState(0)
   const [count,setCount] = useState(0)
   useEffect(()=>{
     setInterval(()=>{
-      console.log(countRef.current)
-      //count.num +=1
       countRef.current++
       setCount(countRef.current)
     },1000)
   },[])
 
-  const clicked = () =>{
-    setTimeout(()=>{ alert(count)},6000)
-
-
-  }
   return <>
-    <div>{countRef.current}</div>
-    <button onClick={clicked}>Click me</button>
+    <div>{count}</div>
   </>
 }
 
