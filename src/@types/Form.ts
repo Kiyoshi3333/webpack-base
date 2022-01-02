9573db6047aa7b5b37dbc757c296365cbdec78d5
Kiyoshi3333 <eemcsquared@gmail.com>
2022-01-02 21:40:27 +0900

    Add comments

--- src/@types/Form.ts
+++ src/@types/Form.ts
@@ -1,5 +1,6 @@
 /*
- * d.ts だと export や declare がいらない importは必要 ただし const foo にはexport/declareが必要になる上に
+ * @types以下のファイルが自動で読み込まれるのはimport文などが無いときだけ。なぜかは不明
+ * @types/*d.ts だと export や declare がいらない importは必要 ただし const foo にはexport/declareが必要になる上に
  *  'const' initializer in an ambient context must be a string or numeric literal or literal enum reference.がでる
  */
 import React from 'react'
